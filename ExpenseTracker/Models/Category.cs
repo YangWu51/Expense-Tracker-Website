032181c8de99dd43a946a3ad71937e25f31648de
Yang Wu <150302384+YangWu51@users.noreply.github.com>
2024-04-15 22:15:49 -0500

    create Category.cs

---
+++ ExpenseTracker/Models/Category.cs
@@ -0,0 +1,18 @@
+namespace ExpenseTracker.Models
+{
+    public class Category{
+        [key]
+        public int CategoryId {get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Title {get; set; }
+
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string Icon {get; set; }
+
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string Type {get; set; }
+    }
+}
